3ce9c14258d55f1fabca31046ecfa7f4263f6dd7
yanggak12 <yanggak12@naver.com>
2021-11-02 01:32:50 +0900

    BodyParser로 회원가입 구현

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express') 
 const app = express() 
 const port = 5000 
+const bodyParser = require('body-parser');
+const {User} = require('./models/User');
+
+app.use(bodyParser.urlencoded({extended: true})); // application/x-www-form-urlencoded
+app.use(bodyParser.json()); // application/json
 
 const mongoose = require('mongoose');
 const mongoURL = 'mongodb+srv://yys:4532@ys.gkhum.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
@@ -12,6 +17,16 @@ mongoose
 app.get('/', (req, res) => {
     // 해당 포트 localhost에 Hello World! 를 표시한다.
   res.send('Hello World!') 
+})
+
+app.post('/register', (req, res)=>{
+  // 회원가입 메소드 추가
+  // 회원가입 할때 필요한 정보들을 클라이언트에서 가져오면 그것들을 DB에 넣어준다.
+  const user = new User(req.body);
+  user.save((err,doc)=>{
+    if(err) return res.json({success:false, err});
+    return res.status(200).json({success:true});
+  })
 })
 
 app.listen(port, () => {
