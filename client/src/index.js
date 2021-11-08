520b6edc5e293173462f147dad705679b85e7ca4
yanggak12 <yanggak12@naver.com>
2021-11-08 22:26:40 +0900

    Ffeat : redux

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,27 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "antd/dist/antd.css";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import promiseMiddleWare from "redux-promise";
+import ReduxThunk from "redux-thunk";
+import Reducer from "./_reducers";
+
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleWare,
+  ReduxThunk
+)(createStore);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider
+    store={createStoreWithMiddleware(
+      Reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
 
