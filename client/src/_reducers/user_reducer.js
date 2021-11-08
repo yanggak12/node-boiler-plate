597997b1f9468b6b220e0fd935c868136e4b46f2
yanggak12 <yanggak12@naver.com>
2021-11-09 01:11:04 +0900

    feat : Register Page

--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -1,10 +1,11 @@
-import { LOGIN_USER } from "../_actions/types";
+import { LOGIN_USER, REGISTER_USER } from "../_actions/types";
 
 export default function user_reducer(prevState = {}, action) {
   switch (action.type) {
     case LOGIN_USER:
       return { ...prevState, loginSuccess: action.payload };
-
+    case REGISTER_USER:
+      return { ...prevState, register: action.payload };
     default:
       return prevState;
   }
--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -1,7 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { registerUser } from "../../../_actions/user_action";
+import { useNavigate } from "react-router-dom";
 
-function RegisterPage() {
-  return <div>RegisterPage</div>;
+function RegisterPage(props) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+  const [Name, setName] = useState("");
+  const [ConfirmPassword, setConfirmPassword] = useState("");
+
+  const onEmailHandler = (event) => {
+    setEmail(event.currentTarget.value);
+  };
+  const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value);
+  };
+  const onNameHandler = (event) => {
+    setName(event.currentTarget.value);
+  };
+  const onConfirmPasswordHandler = (event) => {
+    setConfirmPassword(event.currentTarget.value);
+  };
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    let body = {
+      name: Name,
+      email: Email,
+      password: Password,
+    };
+    dispatch(registerUser(body)).then((response) => {
+      if (response.payload.success) {
+        alert("Success");
+        navigate(-1);
+      } else {
+        alert("error");
+      }
+    });
+  };
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={onSubmitHandler}
+      >
+        <label>Email</label>
+        <input type="email" value={Email} onChange={onEmailHandler} />
+        <label>Name</label>
+        <input type="text" value={Name} onChange={onNameHandler} />
+        <label>Password</label>
+        <input type="password" value={Password} onChange={onPasswordHandler} />
+        <label>Confirm Password</label>
+        <input
+          type="password"
+          value={ConfirmPassword}
+          onChange={onConfirmPasswordHandler}
+        />
+        <br />
+        <button>Sign Up</button>
+      </form>
+    </div>
+  );
 }
 
 export default RegisterPage;
