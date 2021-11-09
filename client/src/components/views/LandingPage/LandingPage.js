2e81dcc6fe8071ab5bcef16d53357a9bcb83e32d
yanggak12 <yanggak12@naver.com>
2021-11-09 15:19:22 +0900

    feat : log out

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,13 +1,24 @@
 import React, { useEffect } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router";
 
 function LandingPage() {
+  const navigate = useNavigate();
   useEffect(() => {
     axios
       .get("/api/hello")
       .then((response) => console.log(response))
       .catch((err) => console.error(err));
   }, []);
+  const onClickHandler = () => {
+    axios.get("/api/user/logout").then((response) => {
+      if (response.data.success) {
+        navigate(-1);
+      } else {
+        alert("Fail to log out.");
+      }
+    });
+  };
   return (
     <div
       style={{
@@ -16,9 +27,11 @@ function LandingPage() {
         alignItems: "center",
         width: "100%",
         height: "100vh",
+        flexDirection: "column",
       }}
     >
       시작 페이지
+      <button onClick={onClickHandler}>Log Out</button>
     </div>
   );
 }
